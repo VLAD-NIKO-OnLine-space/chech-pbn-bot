a9d8aebeed386981a815c68442293104774c9d2a
VLAD-NIKO-OnLine-space <133751481+VLAD-NIKO-OnLine-space@users.noreply.github.com>
2025-06-13 09:52:56 +0300

    Update check_domains.py

--- check_domains.py
+++ check_domains.py
@@ -32,8 +32,19 @@ def get_status_description(status_code):
 
 def check_domain(domain):
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Cache-Control": "no-cache"
+       "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/123.0.0.0 Safari/537.36"
+        ),
+        "Accept": (
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        ),
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
     }
     try:
         response = requests.get(
